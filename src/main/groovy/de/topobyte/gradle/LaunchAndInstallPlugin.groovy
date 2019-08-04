968e604ef7d1da63fb4983f8a640a0d2b5a94d70
Sebastian Kürten <sebastian@topobyte.de>
2019-08-04 12:28:32 +0200

    Create additonal file 'install.sh' to simplify top-level 'install.sh'

--- src/main/groovy/de/topobyte/gradle/LaunchAndInstallPlugin.groovy
+++ src/main/groovy/de/topobyte/gradle/LaunchAndInstallPlugin.groovy
@@ -10,15 +10,19 @@ class LaunchAndInstallPlugin implements Plugin<Project> {
     void apply(Project project) {
         def extension = project.extensions.create("launchAndInstall", LaunchAndInstallExtension)
 
-        project.task('postInstallScript') {
+        project.task('setupScripts') {
             doLast {
                 if (extension.installationPath == null) {
                     throw new InvalidUserDataException("You need to specify the installationPath")
                 }
                 def baseName = "$project.distributions.main.baseName"
+                def installationPath = "\$HOME/share/$extension.installationPath/$baseName-snapshot"
 
-                def file = new File("$project.buildDir/setup/post-install.sh")
-                createPostInstallScript(project, file, "~/share/$extension.installationPath/$baseName-snapshot")
+                def file1 = new File("$project.buildDir/setup/install.sh")
+                createInstallScript(project, file1, baseName, installationPath)
+
+                def file2 = new File("$project.buildDir/setup/post-install.sh")
+                createPostInstallScript(project, file2, installationPath)
             }
         }
 
@@ -30,7 +34,7 @@ class LaunchAndInstallPlugin implements Plugin<Project> {
                 def baseName = "$project.distributions.main.baseName"
 
                 def file = new File("$project.buildDir/makeself/install.sh")
-                createInstallScript(project, file, "\$HOME/share/$extension.installationPath/$baseName-$project.version")
+                createFullInstallScript(project, file, "\$HOME/share/$extension.installationPath/$baseName-$project.version")
             }
         }
 
@@ -69,7 +73,19 @@ class LaunchAndInstallPlugin implements Plugin<Project> {
         makeExecutable(project, file)
     }
 
-    def createInstallScript(project, file, path) {
+    def createInstallScript(project, file, baseName, path) {
+        newFile(project, file)
+        header(file)
+        file.append("DIR=\$(dirname \$0)\n")
+        file.append("\n")
+        file.append("TARGET=\"$path\"\n")
+        file.append("\n")
+        file.append("mkdir -p \"\$TARGET\"\n")
+        file.append("rsync -av --delete \"\$DIR/../install/$baseName/\" \"\$TARGET\"\n")
+        makeExecutable(project, file)
+    }
+
+    def createFullInstallScript(project, file, path) {
         newFile(project, file)
         header(file)
         file.append("DIR=\$(dirname \$0)\n")
@@ -99,10 +115,10 @@ class LaunchAndInstallPlugin implements Plugin<Project> {
     }
 
     def scriptLinks(project, file, path) {
-        file.append("mkdir -p ~/bin\n")
+        file.append("mkdir -p \"\$HOME/bin\"\n")
         file.append("\n")
         project.scriptNames.each {
-            file.append("ln -fsr $path/bin/$it ~/bin/$it\n")
+            file.append("ln -fsr \"$path/bin/$it\" \"\$HOME/bin/$it\"\n")
         }
     }
 
