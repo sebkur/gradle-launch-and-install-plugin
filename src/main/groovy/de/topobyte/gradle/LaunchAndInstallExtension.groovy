47a32709dfe574550e13de0d98055c3056179645
Sebastian Kürten <sebastian@topobyte.de>
2022-05-06 08:33:11 +0200

    Format using Eclipse

--- src/main/groovy/de/topobyte/gradle/LaunchAndInstallExtension.groovy
+++ src/main/groovy/de/topobyte/gradle/LaunchAndInstallExtension.groovy
@@ -1,41 +1,40 @@
 package de.topobyte.gradle;
 
-import org.gradle.jvm.application.tasks.CreateStartScripts
 import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.jvm.application.tasks.CreateStartScripts
 
 class LaunchAndInstallExtension {
 
-    String installationPath
-    String makeselfLabel
+	String installationPath
+	String makeselfLabel
 
-    def createScript(project, mainClass, name) {
-        createScript(project, mainClass, name, null)
-    }
+	def createScript(project, mainClass, name) {
+		createScript(project, mainClass, name, null)
+	}
 
-    def createScript(project, mainClassName, name, options) {
-        project.tasks.create(name: name, type: CreateStartScripts) {
-            outputDir = new File(project.buildDir, 'scripts')
-            mainClass = mainClassName
-            applicationName = name
-            classpath = project.tasks.startScripts.classpath
-            if (!project.hasProperty('scriptNames')) {
-                project.ext.scriptNames = []
-            }
-            project.ext.scriptNames.add(name)
-            if (options != null) {
-                defaultJvmOpts = options
-                doLast {
-                    unixScript.text = unixScript.text.replaceAll('APP_HOME_PLACEHOLDER', '\\\$APP_HOME')
-                }
-            }
-        }
+	def createScript(project, mainClassName, name, options) {
+		project.tasks.create(name: name, type: CreateStartScripts) {
+			outputDir = new File(project.buildDir, 'scripts')
+			mainClass = mainClassName
+			applicationName = name
+			classpath = project.tasks.startScripts.classpath
+			if (!project.hasProperty('scriptNames')) {
+				project.ext.scriptNames = []
+			}
+			project.ext.scriptNames.add(name)
+			if (options != null) {
+				defaultJvmOpts = options
+				doLast {
+					unixScript.text = unixScript.text.replaceAll('APP_HOME_PLACEHOLDER', '\\\$APP_HOME')
+				}
+			}
+		}
 
-        project.tasks[name].dependsOn(project.jar)
+		project.tasks[name].dependsOn(project.jar)
 
-        project.distributions.main.contents.from(project.tasks[name]) {
-             duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-             into 'bin'
-        }
-    }
-
+		project.distributions.main.contents.from(project.tasks[name]) {
+			duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+			into 'bin'
+		}
+	}
 }
--- src/main/groovy/de/topobyte/gradle/LaunchAndInstallPlugin.groovy
+++ src/main/groovy/de/topobyte/gradle/LaunchAndInstallPlugin.groovy
@@ -1,125 +1,126 @@
 package de.topobyte.gradle;
 
-import org.gradle.api.Project
-import org.gradle.api.Plugin
 import org.gradle.api.InvalidUserDataException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 import org.gradle.api.tasks.Sync
 
 class LaunchAndInstallPlugin implements Plugin<Project> {
 
-    void apply(Project project) {
-        def extension = project.extensions.create("launchAndInstall", LaunchAndInstallExtension)
+	void apply(Project project) {
+		def extension = project.extensions.create("launchAndInstall", LaunchAndInstallExtension)
 
-        project.task('setupScripts') {
-            doLast {
-                if (extension.installationPath == null) {
-                    throw new InvalidUserDataException("You need to specify the installationPath")
-                }
-                def baseName = project.distributions.main.distributionBaseName.get()
-                def installationPath = "\$HOME/share/$extension.installationPath/$baseName-snapshot"
+		project.task('setupScripts') {
+			doLast {
+				if (extension.installationPath == null) {
+					throw new InvalidUserDataException("You need to specify the installationPath")
+				}
+				def baseName = project.distributions.main.distributionBaseName.get()
+				def installationPath = "\$HOME/share/$extension.installationPath/$baseName-snapshot"
 
-                def file1 = new File("$project.buildDir/setup/install.sh")
-                createInstallScript(project, file1, baseName, installationPath)
+				def file1 = new File("$project.buildDir/setup/install.sh")
+				createInstallScript(project, file1, baseName, installationPath)
 
-                def file2 = new File("$project.buildDir/setup/post-install.sh")
-                createPostInstallScript(project, file2, installationPath)
-            }
-        }
+				def file2 = new File("$project.buildDir/setup/post-install.sh")
+				createPostInstallScript(project, file2, installationPath)
+			}
+		}
 
-        project.task('makeselfInstallScript') {
-            doLast {
-                if (extension.installationPath == null) {
-                    throw new InvalidUserDataException("You need to specify the installationPath")
-                }
-                def baseName = project.distributions.main.distributionBaseName.get()
+		project.task('makeselfInstallScript') {
+			doLast {
+				if (extension.installationPath == null) {
+					throw new InvalidUserDataException("You need to specify the installationPath")
+				}
+				def baseName = project.distributions.main.distributionBaseName.get()
 
-                def file = new File("$project.buildDir/makeself/install.sh")
-                createFullInstallScript(project, file, "\$HOME/share/$extension.installationPath/$baseName-$project.version")
-            }
-        }
+				def file = new File("$project.buildDir/makeself/install.sh")
+				createFullInstallScript(project, file, "\$HOME/share/$extension.installationPath/$baseName-$project.version")
+			}
+		}
 
-        project.tasks.create(name: "makeselfData", type: Sync) {
-            with project.tasks.installDist
-            into "$project.buildDir/makeself/files"
-        }
+		project.tasks.create(name: "makeselfData", type: Sync) {
+			with project.tasks.installDist
+			into "$project.buildDir/makeself/files"
+		}
 
-        project.tasks.create(name: "makeselfArchive") {
-            dependsOn "makeselfData"
-            dependsOn "makeselfInstallScript"
-            doLast {
-                if (extension.makeselfLabel == null) {
-                    throw new InvalidUserDataException("You need to specify the makeselfLabel")
-                }
-                makeself(project, extension.makeselfLabel)
-            }
-        }
+		project.tasks.create(name: "makeselfArchive") {
+			dependsOn "makeselfData"
+			dependsOn "makeselfInstallScript"
+			doLast {
+				if (extension.makeselfLabel == null) {
+					throw new InvalidUserDataException("You need to specify the makeselfLabel")
+				}
+				makeself(project, extension.makeselfLabel)
+			}
+		}
 
-        project.tasks.create(name: "makeself") {
-            dependsOn "makeselfArchive"
-        }
-    }
+		project.tasks.create(name: "makeself") {
+			dependsOn "makeselfArchive"
+		}
+	}
 
-    def makeself(project, label) {
-        def baseName = project.distributions.main.distributionBaseName.get()
-        project.exec { executable 'makeself' args "$project.buildDir/makeself",
-                       "$project.buildDir/$baseName-${project.version}.run",
-                       label, "./install.sh"}
-    }
+	def makeself(project, label) {
+		def baseName = project.distributions.main.distributionBaseName.get()
+		project.exec {
+			executable 'makeself' args "$project.buildDir/makeself",
+					"$project.buildDir/$baseName-${project.version}.run",
+					label, "./install.sh"
+		}
+	}
 
-    def createPostInstallScript(project, file, path) {
-        newFile(project, file)
-        header(file)
-        scriptLinks(project, file, path)
-        makeExecutable(project, file)
-    }
+	def createPostInstallScript(project, file, path) {
+		newFile(project, file)
+		header(file)
+		scriptLinks(project, file, path)
+		makeExecutable(project, file)
+	}
 
-    def createInstallScript(project, file, baseName, path) {
-        newFile(project, file)
-        header(file)
-        file.append("DIR=\$(dirname \$0)\n")
-        file.append("\n")
-        file.append("TARGET=\"$path\"\n")
-        file.append("\n")
-        file.append("mkdir -p \"\$TARGET\"\n")
-        file.append("rsync -av --delete \"\$DIR/../install/$baseName/\" \"\$TARGET\"\n")
-        makeExecutable(project, file)
-    }
+	def createInstallScript(project, file, baseName, path) {
+		newFile(project, file)
+		header(file)
+		file.append("DIR=\$(dirname \$0)\n")
+		file.append("\n")
+		file.append("TARGET=\"$path\"\n")
+		file.append("\n")
+		file.append("mkdir -p \"\$TARGET\"\n")
+		file.append("rsync -av --delete \"\$DIR/../install/$baseName/\" \"\$TARGET\"\n")
+		makeExecutable(project, file)
+	}
 
-    def createFullInstallScript(project, file, path) {
-        newFile(project, file)
-        header(file)
-        file.append("DIR=\$(dirname \$0)\n")
-        file.append("\n")
-        file.append("TARGET=\"$path\"\n")
-        file.append("\n")
-        file.append("mkdir -p \"\$TARGET\"\n")
-        file.append("rsync -av --delete \"\$DIR/files/\" \"\$TARGET\"\n")
-        file.append("\n")
-        scriptLinks(project, file, path)
-        makeExecutable(project, file)
-    }
+	def createFullInstallScript(project, file, path) {
+		newFile(project, file)
+		header(file)
+		file.append("DIR=\$(dirname \$0)\n")
+		file.append("\n")
+		file.append("TARGET=\"$path\"\n")
+		file.append("\n")
+		file.append("mkdir -p \"\$TARGET\"\n")
+		file.append("rsync -av --delete \"\$DIR/files/\" \"\$TARGET\"\n")
+		file.append("\n")
+		scriptLinks(project, file, path)
+		makeExecutable(project, file)
+	}
 
-    def newFile(project, file) {
-        def dir = file.getParent()
-        project.mkdir dir
-        project.delete file
-    }
+	def newFile(project, file) {
+		def dir = file.getParent()
+		project.mkdir dir
+		project.delete file
+	}
 
-    def makeExecutable(project, file) {
-        project.exec { executable 'chmod' args '+x', file  }
-    }
+	def makeExecutable(project, file) {
+		project.exec { executable 'chmod' args '+x', file  }
+	}
 
-    def header(file) {
-        file.append("#!/bin/bash\n")
-        file.append("\n")
-    }
+	def header(file) {
+		file.append("#!/bin/bash\n")
+		file.append("\n")
+	}
 
-    def scriptLinks(project, file, path) {
-        file.append("mkdir -p \"\$HOME/bin\"\n")
-        file.append("\n")
-        project.scriptNames.each {
-            file.append("ln -fsr \"$path/bin/$it\" \"\$HOME/bin/$it\"\n")
-        }
-    }
-
+	def scriptLinks(project, file, path) {
+		file.append("mkdir -p \"\$HOME/bin\"\n")
+		file.append("\n")
+		project.scriptNames.each {
+			file.append("ln -fsr \"$path/bin/$it\" \"\$HOME/bin/$it\"\n")
+		}
+	}
 }
